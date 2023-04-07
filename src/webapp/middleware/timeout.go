e10a7a30e77b879e732e36ccf51485590ab861e0
Vivek Mishra <v.mishra@f5.com>
2023-04-07 18:46:53 +0530

    added connection to database

--- src/webapp/middleware/timeout.go
+++ src/webapp/middleware/timeout.go
@@ -16,18 +16,21 @@ func (tm TimeoutMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx := r.Context()
-	ctx, _ = context.WithTimeout(ctx, 3*time.Second)
+	ctx, _ = context.WithTimeout(ctx, 3 * time.Second)
+	// replace request with new context
 	r.WithContext(ctx)
+
 	ch := make(chan struct{})
-	go func() {
+
+	go func ()  {
 		tm.Next.ServeHTTP(w, r)
 		ch <- struct{}{}
 	}()
+
 	select {
-	case <-ch:
-		return
-	case <-ctx.Done():
+	case <- ch: 
+	return
+	case <- ctx.Done():
 		w.WriteHeader(http.StatusRequestTimeout)
 	}
-
 }
